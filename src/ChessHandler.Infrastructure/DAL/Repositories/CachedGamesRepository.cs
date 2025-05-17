5a05e0a0d71f02245ff664769e2b86a402a1346a
michal2612 <retrospektywny@gmail.com>
2025-05-17 12:28:33 +0100

    Added support for Redis

--- src/ChessHandler.Infrastructure/DAL/Repositories/CachedGamesRepository.cs
+++ src/ChessHandler.Infrastructure/DAL/Repositories/CachedGamesRepository.cs
@@ -1,22 +1,30 @@
 using ChessHandler.Core.Entities;
 using ChessHandler.Core.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
 
 namespace ChessHandler.Infrastructure.DAL.Repositories;
 
 internal sealed class CachedGamesRepository(IGamesRepository decorated,
-    IMemoryCache cache)
+    IDistributedCache distributedCache)
     : IGamesRepository
 {
-    public Task<Game> GetAsync(int gameId)
+    public async Task<Game> GetAsync(int gameId)
     {
         var key = $"game-{gameId}";
+        string? cachedMember = await distributedCache.GetStringAsync(key);
 
-        return cache.GetOrCreateAsync(key, entry =>
-        {
-            entry.SetAbsoluteExpiration(TimeSpan.FromHours(2));
-            return decorated.GetAsync(gameId);
-        });
+        if (!string.IsNullOrEmpty(cachedMember))
+            return JsonConvert.DeserializeObject<Game>(cachedMember)!;
+        
+        var game = await decorated.GetAsync(gameId);
+
+        if (game == null)
+            return game;
+        
+        await distributedCache.SetStringAsync(key, JsonConvert.SerializeObject(game));
+        return game;
     }
 
     public Task<IEnumerable<Game>> GetAllAsync(DateTime since, uint max = 100)
